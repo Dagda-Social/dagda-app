184f724ebec349056c09986eb31d637b23f49b95
D4vidDf <david@d4viddf.com>
2022-11-19 21:46:23 +0100

    Fixed background

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Stack(children: <Widget>[
         Center(
           child: Column(
