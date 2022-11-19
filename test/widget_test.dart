72b68177efaeaa6aa39162d77651c67d299e5466
D4vidDf <david@d4viddf.com>
2022-11-19 15:08:20 +0100

    Added test for main

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,4 +10,14 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:dagda/main.dart';
 
-void main() {}
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('Building the next generation of Social Network.'), findsOneWidget);
+    expect(find.text('0'), findsNothing);
+
+  });
+}
