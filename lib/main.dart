b4aec0a47dcb4171daaafa0a1d8b4f042f745c65
D4vidDf <david@d4viddf.com>
2022-11-22 01:30:45 +0100

    Fixed redirect and mobile crash

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:dagda/register.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 import 'login.dart';
 
 void main() {
-  usePathUrlStrategy();
   runApp(const MyApp());
 }
 
