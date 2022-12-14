56dfbf99625a2e254d80cca4c615e2042db5d63f
D4vidDf <david@d4viddf.com>
2022-12-14 21:05:33 +0100

    Fixed meta

--- lib/meta_strategy/web_meta_strategy.dart
+++ lib/meta_strategy/web_meta_strategy.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 
 void configMeta(Locale locale) {
   MetaElement langMeta = MetaElement();
-  langMeta.content = locale.languageCode.toString();
   langMeta.name = 'lang';
+  langMeta.content = locale.languageCode.toString();
   document.head!.append(langMeta);
 }
