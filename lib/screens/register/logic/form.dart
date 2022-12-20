6f49db23dd1a952d12472e8c3b31520ff54be707
D4vidDf <david@d4viddf.com>
2022-12-20 21:45:45 +0100

    Preload canvaskit for better performance

--- lib/screens/register/logic/form.dart
+++ lib/screens/register/logic/form.dart
@@ -3,9 +3,9 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 checkMail(String mail, BuildContext context) {
   if (mail.isEmpty || mail == null) {
-    return AppLocalizations.of(context)!.enterEmail;
+    return AppLocalizations.of(context).enterEmail;
   } else if (!mail.contains('@')) {
-    return AppLocalizations.of(context)!.errorEmail;
+    return AppLocalizations.of(context).errorEmail;
   } else {
     return null;
   }
@@ -13,24 +13,24 @@ checkMail(String mail, BuildContext context) {
 
 checkPasswordWithSpecialCharacters(String password, BuildContext context) {
   if (password.isEmpty || password == null) {
-    return AppLocalizations.of(context)!.enterPassword;
+    return AppLocalizations.of(context).enterPassword;
   } else if (password.length < 8) {
-    return AppLocalizations.of(context)!.errorPassword;
+    return AppLocalizations.of(context).errorPassword;
   } else if (!password.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
-    return AppLocalizations.of(context)!.errorPasswordWithSpecialCharacters;
+    return AppLocalizations.of(context).errorPasswordWithSpecialCharacters;
   } else {
     return null;
   }
 }
 
-checkSamePassword(String password, String passwordConfirm, BuildContext context) {
+checkSamePassword(
+    String password, String passwordConfirm, BuildContext context) {
   if (passwordConfirm.isEmpty || passwordConfirm == null) {
-    return AppLocalizations.of(context)!.enterConfirmPassword;
-  } else if(checkPasswordWithSpecialCharacters(password, context)!=null){
+    return AppLocalizations.of(context).enterConfirmPassword;
+  } else if (checkPasswordWithSpecialCharacters(password, context) != null) {
     return checkPasswordWithSpecialCharacters(password, context);
-  }
-   else if (password != passwordConfirm) {
-    return AppLocalizations.of(context)!.errorConfirmPassword;
+  } else if (password != passwordConfirm) {
+    return AppLocalizations.of(context).errorConfirmPassword;
   } else {
     return null;
   }
